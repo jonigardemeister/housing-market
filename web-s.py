b27cd2fa4320517f0754ac4b31b7ef5ccc776022
Joni Gardemeister <gardemeisterjoni@gmail.com>
2023-12-23 19:38:34 +0200

    Update solution

--- web-s.py
+++ web-s.py
@@ -2,14 +2,37 @@
 
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.by import By
 from bs4 import BeautifulSoup
 import pandas as pd
 
-df_cities = pd.read_csv('cities.csv', encoding='utf-8')
+from datetime import datetime
+
+def fetch_rows_from_page(soup):
+    result = list()
+
+    table = soup.find('table', {"id": "mainTable"})
+    rows = table.find_all('tr')
+
+    for tr in rows:
+        td = tr.find_all('td')
+        row = [i.text for i in td]
+        row.append(city)
+        if(len(row) == 13):
+            result.append(row)
+            
+    return result
+
+
+now = datetime.now()
+
+now_date = now.strftime("%d_%m_%Y")
+
+df_cities = pd.read_csv('data/cities.csv', encoding='utf-8')
 row_list = list()
 columns = ['Kaupunginosa','Huoneisto','Talot','m2','hinta','e/m2','Rakennusvuosi','Kerros','Hissi','Kunto','Tontti','Energialuokka','Kunta']
 
-driver = webdriver.Chrome("/Library/Frameworks/Python.framework/Versions/3.9/bin/chromedriver")
+driver = webdriver.Chrome()
 
 for city in df_cities['Kunta']:
     print(city)
@@ -20,26 +43,19 @@ for city in df_cities['Kunta']:
     index = 0
 
     while(soup.find('input',{'class': 'submit', 'value':'seuraava sivu »'}) and index < 100):
-        table = soup.find('table', {"id": "mainTable"})
-        rows = table.find_all('tr')
-
-        for tr in rows:
-            td = tr.find_all('td')
-            row = [i.text for i in td]
-            if(len(row) == 12):
-                row_list.append(row)
+        
+        row_list.extend(fetch_rows_from_page(soup))
 
         try:
-            button = driver.find_element_by_xpath("//input[@value='seuraava sivu »']")
+            button = driver.find_element(By.XPATH, "//input[@value='seuraava sivu »']")
             picture = button.click()
         except NoSuchElementException:
             break
 
-        df = pd.DataFrame(row_list, columns=columns)
-        print(df.shape)
-        df.to_csv('beautifulsoup.csv', index=False, encoding='utf-8-sig')
         index += 1
 
-df = pd.DataFrame(row_list,columns=columns)
+    row_list.extend(fetch_rows_from_page(soup))
+
+df = pd.DataFrame(row_list, columns=columns)
 df.dropna(subset=columns, inplace=True)
-df.to_csv('beautifulsoup.csv', index=False, encoding='utf-8-sig')
+df.to_csv(f'data/results/data_{now_date}.csv', index=False, encoding='utf-8-sig')
